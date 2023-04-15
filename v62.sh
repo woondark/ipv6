c18c361f3d90567272644a9de3599aee076c9abc
namhv <namclls@gmail.com>
2023-04-16 01:39:07 +0700

    Update v62.sh

--- v62.sh
+++ v62.sh
@@ -83,19 +83,8 @@ LAST_PORT=40400
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-echo NM_CONTROLLED="no" >> /etc/sysconfig/network-scripts/ifcfg-${main_interface}
-chmod +x $WORKDIR/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
-
-cat >>/etc/rc.local <<EOF
-#systemctl start NetworkManager.service
-# ifup ${main_interface}
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 65535
-/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
-EOF
 
 bash /etc/rc.local
 
